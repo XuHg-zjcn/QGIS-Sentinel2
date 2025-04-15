743a9b5f4d3504b4246ad45521bff557c408a4c1
徐瑞骏 <1687701765@qq.com>
2025-04-15 16:15:23 +0800

    添加GPLv3许可证

--- merge.sh
+++ merge.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/bash
+########################################################################
+# Sentinel-2 imagery uncompress and merge to gdal vrt script
+# Copyright (C) 2024-2025  Xu Ruijun
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+########################################################################
 uncomp_path=""
 if [ ! -e $1 ];then #文件或文件夹不存在
     echo $1 is not exists
