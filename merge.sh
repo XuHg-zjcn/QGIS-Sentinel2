0464b09812ca3ad2bc96b3d3c955bdc5741bb11f
徐瑞骏 <1687701765@qq.com>
2025-04-15 21:40:55 +0800

    跳过已存在的vrt和vrt.ovr，不进行重建

--- merge.sh
+++ merge.sh
@@ -28,7 +28,7 @@ elif [[ -f $1 && ${1##*.} = 'zip' ]];then #第一个参数是zip压缩包路径
     if [ ! -d $uncomp_path ];then #解压路径不是一个文件夹
         unzip $1 #进行解压
     else
-        echo already decompressed in $uncom_path
+        echo already decompressed in $uncomp_path
     fi
 elif [ ! -d $1 ];then
     echo $1 is not directory or zip file
@@ -37,55 +37,72 @@ else
     uncomp_path=$1
 fi
 
-name=""
-for dir in $(ls $uncomp_path/GRANULE)
-do
-    path=$uncomp_path/GRANULE/$dir
-    if [ ! -d $path ];then
-        continue
-    fi
-    if [ ! -z $name ];then
-        echo mutiliy dirctory in $uncomp_path/GRANULE
-        exit
+buildvrt() {
+    uncomp_path=$1
+    output_path=$2
+    name=""
+    for dir in $(ls $uncomp_path/GRANULE)
+    do
+        path=$uncomp_path/GRANULE/$dir
+        if [ ! -d $path ];then
+            continue
+        fi
+        if [ ! -z $name ];then
+            echo mutiliy dirctory in $uncomp_path/GRANULE
+            exit
+        else
+            name="$dir"
+        fi
+    done
+
+    img_dir=$uncomp_path/GRANULE/$name/IMG_DATA
+    if [ -d $img_dir/R10m ];then
+        ls -1 -f $img_dir/R20m/*_B01_20m.jp2\
+                 $img_dir/R10m/*_B02_10m.jp2\
+                 $img_dir/R10m/*_B03_10m.jp2\
+                 $img_dir/R10m/*_B04_10m.jp2\
+                 $img_dir/R20m/*_B05_20m.jp2\
+                 $img_dir/R20m/*_B06_20m.jp2\
+                 $img_dir/R20m/*_B07_20m.jp2\
+                 $img_dir/R10m/*_B08_10m.jp2\
+                 $img_dir/R60m/*_B09_60m.jp2\
+                 $img_dir/R60m/*_WVP_60m.jp2\
+                 $img_dir/R20m/*_B11_20m.jp2\
+                 $img_dir/R20m/*_B12_20m.jp2\
+                 $img_dir/R20m/*_B8A_20m.jp2\
+        > input_files.txt
     else
-        name="$dir"
+        ls $img_dir/*_B*.jp2 > input_files.txt
     fi
-done
 
-img_dir=$uncomp_path/GRANULE/$name/IMG_DATA
-if [ -d $img_dir/R10m ];then
-    ls -1 -f $img_dir/R20m/*_B01_20m.jp2\
-             $img_dir/R10m/*_B02_10m.jp2\
-             $img_dir/R10m/*_B03_10m.jp2\
-             $img_dir/R10m/*_B04_10m.jp2\
-             $img_dir/R20m/*_B05_20m.jp2\
-             $img_dir/R20m/*_B06_20m.jp2\
-             $img_dir/R20m/*_B07_20m.jp2\
-             $img_dir/R10m/*_B08_10m.jp2\
-             $img_dir/R60m/*_B09_60m.jp2\
-             $img_dir/R60m/*_WVP_60m.jp2\
-             $img_dir/R20m/*_B11_20m.jp2\
-             $img_dir/R20m/*_B12_20m.jp2\
-             $img_dir/R20m/*_B8A_20m.jp2\
-    > input_files.txt
-else
-    ls $img_dir/*_B*.jp2 > input_files.txt
-fi
+    count=$(wc -l input_files.txt)
+    if [ ${count% *} -ne "13" ];
+    then
+        echo not 13 files
+        exit
+    fi
 
-count=$(wc -l input_files.txt)
-if [ ${count% *} -ne "13" ];
-then
-    echo not 13 files
-    exit
-fi
+    echo "using these files:"
+    cat input_files.txt
 
-echo "using these files:"
-cat input_files.txt
+    echo
 
-echo
+    echo "ouput path:" $output_path
+
+    gdalbuildvrt -overwrite -resolution highest -separate -r cubic -input_file_list input_files.txt $output_path
+}
 
 output_path=$uncomp_path/$(echo $uncomp_path | awk  '{print substr($uncomp_path,1,3)"_"substr($uncomp_path,12,8)}').vrt
-echo "ouput path:" $output_path
 
-gdalbuildvrt -overwrite -resolution highest -separate -r cubic -input_file_list input_files.txt $output_path
-gdaladdo $output_path -r average -ro 2 4 8 16
+#TODO: 应识别上次构建时是否中断，并重建
+if [ ! -f $output_path ];then
+    buildvrt $uncomp_path $output_path
+else
+    echo "already build vrt"
+fi
+
+if [ ! -f $output_path.ovr ];then
+    gdaladdo $output_path -r average -ro 2 4 8 16
+else
+    echo "already build ovr"
+fi
